6fa177584e5363ff5bfab90ab20e89a56611aa07
MarcosAndre28 <34040590+MarcosAndre28@users.noreply.github.com>
2020-06-22 20:43:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup(){
 
-    creatCanvas(400, 400);
-    
+    createCanvas(200,200);
+    console.log('setup');
 }
 function draw(){
-background(400);
+background(200);
 
 line(10,10,100,100);
-
+console.log('draw');
 }
