370c3332703f49dc9bbf54fce117a09c57753a28
MarcosAndre28 <34040590+MarcosAndre28@users.noreply.github.com>
2020-06-23 01:25:16 -0300

    Update Codigos

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 
-let imagemCenario;
+let imagemCenario,imagemPersonagem,cenario,somDoJogo,personagem;
 
 
 function preload() {
   imagemCenario = loadImage('assets/imagens/cenario/floresta.png'); 
+  imagemPersonagem = loadImage('assets/imagens/personagem/correndo.png'); 
+  somDoJogo = loadSound('assets/sons/trilha_jogo.mp3');
 }
 
 function setup() {
-  createCanvas( windowWidth, windowHeight);
+    createCanvas( windowWidth, windowHeight);
+    cenario = new Cenario(imagemCenario,5);
+    personagem = new Personagem(imagemPersonagem);
+   frameRate(35);
+    somDoJogo.loop();
+    
 }
 function draw() {
-  background(imagemCenario);
+    cenario.exibe();
+    cenario.move();  
+   personagem.exibe();
+    
 }
