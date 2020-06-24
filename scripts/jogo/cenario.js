8d586f3220a43683e35e3b47dcb22ea013c0054d
MarcosAndre28 <34040590+MarcosAndre28@users.noreply.github.com>
2020-06-23 23:11:06 -0300

    Revert "update de codigo e inimigos"
    This reverts commit 96752e9c026842973cdc89e64d334913b4a7a792.

--- scripts/jogo/cenario.js
+++ scripts/jogo/cenario.js
@@ -1,28 +1,24 @@
-class Cenario {
-    constructor(imagem, velocidade) {
-      this.imagem = imagem;
-      this.velocidade = velocidade;
-      this.x1 = 0;
-      this.x2 = width;
+class Cenario{
+    constructor(imagem,velocidade){
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
     }
+    exibe(){
+    image(this.imagem,this.x1,0,width,height);
+    image(this.imagem,this.x2 +1,0,width,height);
     
-    exibe() {
-      image(this.imagem, this.x1, 0, width, height);
-      image(this.imagem, this.x2, 0, width, height); 
     }
+    move(){
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
     
-    move() {
-      this.x1 = this.x1 - this.velocidade;
-      this.x2 = this.x2 - this.velocidade;
-      
-      // Quando x1 < -width, significa que x1, saiu completamente da tela e deve reaparecer novamente do lado oposto
-      if (this.x1 < -width){
+    if(this.x1 < -width){
         this.x1 = width;
-      }
-      
-      if (this.x2 < -width){
+    }if(this.x2 < -width){
         this.x2 = width;
-      }
     }
     
-  }
+    }
+    }
