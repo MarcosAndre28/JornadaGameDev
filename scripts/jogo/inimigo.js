e269c00d4a857bb007d484baf1a65f775cd78eb2
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2020-06-24 01:00:38 -0300

    Gravidade e Velocidade
    Mudança da matriz do personagem para o sketck.js, comandos exibe e anima subtituidos, aplicação da gravidade e da velocidade do Pulo.  Função para pegar tecla pressionada inserida, matriz personagem virou constante matrizPersonagem, velocidade do inimigo, junto com sua movimentação imlementada.

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -3,9 +3,16 @@ class Inimigo extends Animacao{
 
        super(matriz,imagem,x,largura,altura,larguraSprite,alturaSprite);
 
-
+        this.velocidade = 10;
     }
 
 
+    move() {
+        this.x = this.x - this.velocidade;
+
+        if(this.x < -this.largura){
+            this.x= width;  
+        }
+    }
 
 }
